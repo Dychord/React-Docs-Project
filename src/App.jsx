b97d9036ca57d5207763e918d583505a4fbfe9ca
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-23 19:45:56 +0530

    first Commint

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Background from "./components/Background"
+
+function App() {
+
+  return (
+    <>
+      <div className="relative w-full h-screen selection:text-fuchsia-700 bg-zinc-800">
+        <Background />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+        <div className="absolute top-10 w-full text-xl text-zinc-600 py-10 justify-center items-center flex select-none">Documents</div>
+        <h1 className="text-[18vh] text-zinc-900 font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">Docs.</h1>
+    </>
+  )
+}
+
+export default Background
